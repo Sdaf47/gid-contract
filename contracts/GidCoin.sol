690bfd40b5c243e09322bd5581f7a28fabf7f347
Bulatkin Stanislav <sdaf47@yandex.ru>
2017-07-19 19:36:28 +0300

    remove test-data + init gid coin

---
+++ contracts/GidCoin.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.4;
+
+
+contract GidCoin {
+    mapping (address => uint256) public balanceOf;
+
+    function GidCoin(uint256 initialSupply) {
+        balanceOf[msg.sender] = initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+    }
+}
