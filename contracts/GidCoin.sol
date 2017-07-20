0091022e2e79ea8e7328a629b5d3ee56c8782c2e
Bulatkin Stanislav <sdaf47@yandex.ru>
2017-07-20 13:36:08 +0300

    some thoughts

--- contracts/GidCoin.sol
+++ contracts/GidCoin.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.4;
 
 contract GidCoin {
     mapping (address => uint256) public balanceOf;
+    mapping (address => uint256) public personalData; // TODO which type of data? it is not mapping because relation is one-to-many
 
     function GidCoin(uint256 initialSupply) {
         balanceOf[msg.sender] = initialSupply;
@@ -14,4 +15,9 @@ contract GidCoin {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
     }
+
+    function addPersonalData(string field, string value) {
+        personalData[_owner] = sha256(field + value); // TODO how to calculate a hash from data? and THIS IS NOT-F..G-MAPPING-DATA
+    }
+
 }
