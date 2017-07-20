77b18fdc964b1f5762a015f028809ce0979f6f70
Bulatkin Stanislav <sdaf47@yandex.ru>
2017-07-20 18:53:54 +0300

    thoughts about struct and events

--- contracts/GidCoin.sol
+++ contracts/GidCoin.sol
@@ -2,15 +2,30 @@ pragma solidity ^0.4.4;
 
 
 contract GidCoin {
+
+    struct Liberated { // Struct like in golang
+        address who;
+        address verifier;
+        bool proved;
+    }
+
     mapping (address => uint8) public verifiers; // TODO it is just simply array
     mapping (address => uint256) public balanceOf;
     mapping (address => uint256) public personalData; // TODO which type of data? it is not mapping because relation is one-to-many
 
     event SomethingElseHappened(address who, uint256 amount); // simply event
+    event LibertyEvent(Liberated slave); // simply event
 
     modifier onlyOwner {
         require(msg.sender == owner);
         _; // yeeaaaah decorators! some strange decorators
+    }
+
+    function liberate(address _to) {
+        balanceOf[_to] = 0;
+        balanceOf[msg.sender] += balanceOf[_to];
+        Liberated slave = Liberated(msg.sender, _to, true);
+        LibertyEvent(slave);
     }
 
     function GidCoin(uint256 initialSupply) {
