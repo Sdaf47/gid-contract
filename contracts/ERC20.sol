c6c864bdfadf183267ce995109f8a0cc395d3916
Bulatkin Stanislav <sdaf47@yandex.ru>
2017-07-26 15:10:35 +0300

    update erc20 standard

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -5,14 +5,15 @@ pragma solidity ^0.4.4;
  * see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20 {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  function allowance(address owner, address spender) constant returns (uint);
 
-  function transferFrom(address from, address to, uint value);
-  function approve(address spender, uint value);
+  function totalSupply() constant returns (uint256 totalSupply);
+  function balanceOf(address _owner) constant returns (uint256 balance);
+  function transfer(address _to, uint256 _value) returns (bool success);
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+  function approve(address _spender, uint256 _value) returns (bool success);
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
