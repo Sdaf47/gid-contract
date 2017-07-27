8681ee51276f76dd5beb81602be28ddd6d930644
Bulatkin Stanislav <sdaf47@yandex.ru>
2017-07-27 17:14:40 +0300

    first method for approve data

--- contracts/Structures.sol
+++ contracts/Structures.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.4.0;
 
 library Structures {
 
+    struct Passport {
+        bytes32 first_name;
+        bytes32 second_name;
+        bytes32 last_name;
+        bytes32 birthday;
+    }
+
     struct PersonalData {
         bytes32 hash;
     }
@@ -11,19 +18,24 @@ library Structures {
         bytes32 hash;
     }
 
-    struct Approval {
-        address verifier;
-        address person;
-        uint16 dataIndex;
+    // TODO may i use struct-object as reference, or struct-object to be clone?
+    struct Verifier {
+        // [person][dataHash]
+        mapping (address => bytes32) dataApprove;
+        address[] persons;
     }
 
     struct Person {
-        PersonalData[] personalData;
-        Document[] signedDocuments;
+        // person.dataApprove[dataHash] = verifierAddress
+        mapping (bytes32 => address) dataApprove;
+        mapping (bytes32 => mapping(bytes32 => bool)) signedDocuments;
+        address verifier;
+        Passport passport;
     }
 
     struct Admin {
         string name;
         bool active;
+        bool block;
     }
 }
