b53bb1bc52f3a32cc9e0a6cd70fa562c59ee99c8
sdaf47 <sdaf47@yandex.ru>
2017-08-22 18:26:52 +0300

    testing

--- test/first_test.js
+++ test/first_test.js
@@ -6,103 +6,113 @@ let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 contract('Gid', (accounts) => {
 
     it('crowd-sale test', async () => {
-        let balance = [];
-        let contractBalance = 0;
+
+        let balances = [];
+        let contractBalance;
         const gid = await Gid.deployed();
 
-        console.log('contract address', gid.address);
+        // total supply
         const totalSupply = await gid.totalSupply.call();
-        assert.equal(totalSupply.valueOf(), 100000000, "total supply = 100000000");
+        assert.equal(totalSupply.valueOf(), 100000000000000000000000000, "total supply = 100000000");
 
-        balance[0] = await gid.balanceOf.call(accounts[0]);
-        assert.equal(balance[0].valueOf(), 100000000, "account 1 balance = 100000000");
-
-        assert(accounts.length >= 3, true, "need more accounts");
-
-        await gid.startPreICO(1000000, 100, 50, 300, accounts[2], accounts[1]);
-        const end = await gid.endPreICO();
-
-        console.log('end of pre-ICO', end.valueOf());
-
-        for (let i = 0; i < 3; i++) {
-            logBalance('account ' + i + ' balance:', await web3.eth.getBalance(accounts[i]));
-        }
-
-        console.log('send transaction 0 >> 1');
-        await web3.eth.sendTransaction({from: accounts[0], to: accounts[1], value: toWei(2)});
-
-        for (let i = 0; i < 3; i++) {
-            logBalance('account ' + i + ' balance:', await web3.eth.getBalance(accounts[i]));
-        }
-
-        console.log('send transaction 1 >> gid');
-        await web3.eth.sendTransaction({from: accounts[1], to: gid.address, value: toWei(1)});
-
-        contractBalance = await web3.eth.getBalance(gid.address);
-        assert.equal(contractBalance.valueOf(), toEther(1), "contract balance = 1 ether");
-        logBalance('contract balance:', contractBalance);
-
-        balance[0] = await gid.balanceOf.call(accounts[0]);
-        assert.equal(balance[0].valueOf(), 99999700, "account 1 balance = 99999700");
-        console.log("account 0 balance", balance[0].valueOf(), " gid");
-
-        balance[1] = await gid.balanceOf.call(accounts[1]);
-        assert.equal(balance[1].valueOf(), 300, "account 2 balance = 300");
-        console.log("account 1 balance", balance[1].valueOf(), " gid");
-
-        console.log('send transaction 0 >> 2');
-        await web3.eth.sendTransaction({from: accounts[0], to: accounts[1], value: toWei(2)});
-
-        console.log('send transaction 2 >> gid');
-        await web3.eth.sendTransaction({from: accounts[2], to: gid.address, value: toWei(1.5)});
-
-        contractBalance = await web3.eth.getBalance(gid.address);
-        assert.equal(contractBalance.valueOf(), toEther(2.5), "contract balance = 2.5 ether");
-        logBalance('contract balance:', contractBalance);
-
-        balance[2] = await gid.balanceOf.call(accounts[2]);
-        assert.equal(balance[2].valueOf(), 450, "account 2 balance = 450");
-        console.log("account 2 balance", balance[2].valueOf(), " gid");
-
-        console.log('send transaction 2 >> gid');
-        await web3.eth.sendTransaction({from: accounts[2], to: gid.address, value: toWei(0.1)});
+        // starting balance
+        balances[0] = await gid.balanceOf.call(accounts[0]);
+        assert.equal(balances[0].valueOf(), 100000000000000000000000000, "account 1 balance = 100000000");
 
 
-        contractBalance = await web3.eth.getBalance(gid.address);
-        assert.equal(contractBalance.valueOf(), toEther(2.6), "contract balance = 2.5 ether");
-        logBalance('contract balance:', contractBalance);
+        // PRIVATE FUNDING
+        // start private funding
+        await gid.startPrivateFunding(300, accounts[4], accounts[5]);
 
-        balance[2] = await gid.balanceOf.call(accounts[2]);
-        assert.equal(balance[2].valueOf(), 480, "account 2 balance = 480");
-        console.log("account 2 balance", balance[2].valueOf(), " gid");
+        // 1 >> gid
+        await web3.eth.sendTransaction({from: accounts[1], to: gid.address, value: toWei(1), gas: 3000000});
+        balances[1] = await gid.balanceOf.call(accounts[1]);
+        assert.equal(balances[1].valueOf(), 300000000000000000000, "account 1 balance = 300");
 
-        // todo timer?
+        // 2 >> gid
+        await web3.eth.sendTransaction({from: accounts[2], to: gid.address, value: toWei(1), gas: 3000000});
+        balances[2] = await gid.balanceOf.call(accounts[2]);
+        assert.equal(balances[2].valueOf(), 300000000000000000000, "account 1 balance = 300");
 
-        console.log('complete pre ico');
+        // 0 balance
+        balances[0] = await gid.balanceOf.call(accounts[0]);
+        assert.equal(balances[0].valueOf(), 99999400000000000000000000, "account 1 balance = 99999400");
 
+        // end private funding
+        await gid.completePrivateFunding();
+
+        // todo check negative
+
+
+        // PRE ICO
+        // start pre-ICO
+        await gid.startPreICO(1000000000000000000000000, 5, 300, accounts[4], accounts[5]);
+
+        // 1 >> gid
+        await web3.eth.sendTransaction({from: accounts[1], to: gid.address, value: toWei(1), gas: 3000000});
+        balances[1] = await gid.balanceOf.call(accounts[1]);
+
+        assert.equal(balances[1].valueOf(), 600000000000000000000, "account 1 balance = 600");
+
+        // 2 >> gid
+        await web3.eth.sendTransaction({from: accounts[2], to: gid.address, value: toWei(1), gas: 3000000});
+        balances[2] = await gid.balanceOf.call(accounts[2]);
+        assert.equal(balances[2].valueOf(), 600000000000000000000, "account 1 balance = 600");
+
+        // 0 balance
+        balances[0] = await gid.balanceOf.call(accounts[0]);
+        assert.equal(balances[0].valueOf(), 99998800000000000000000000, "account 1 balance = 99998800");
+
+        // todo check negative
+
+        // end pre ICO
+        let endPre = await gid.endPreICO.call();
+        await sleep(1000 * endPre.valueOf() + 1000);
         await gid.completePreICO();
+        console.log("completePreICO");
+        // todo check negative
 
+        // ICO
+        // start ICO
+        await gid.startICO(300, 10, accounts[4], accounts[5]);
         await web3.eth.sendTransaction({from: accounts[2], to: gid.address, value: toWei(0.1)});
+        console.log("started ICO");
 
+        // 3 >> gid
+        await web3.eth.sendTransaction({from: accounts[3], to: gid.address, value: toWei(1.5), gas: 3000000});
+        balances[3] = await gid.balanceOf.call(accounts[3]);
+        assert.equal(balances[3].valueOf(), 450000000000000000000, "account 3 balance = 450");
+
+        // todo maximum
+
+        // contract balance
         contractBalance = await web3.eth.getBalance(gid.address);
-        logBalance('contract balance:', contractBalance);
+        assert.equal(contractBalance.valueOf(), 1600000000000000000, "contract balance = 1.6");
 
-        balance[2] = await gid.balanceOf.call(accounts[1]);
-        assert.equal(balance[2].valueOf(), 480, "account 2 balance = 450");
-        console.log("account 2 balance", balance[2].valueOf(), " gid");
+        // end ICO
+        let endIco = await gid.endICO.call();
+        await sleep(1000 * endIco.valueOf());
+        console.log("error?");
+        await gid.completeICO();
+        console.log("completeICO");
 
+        // todo negative
+        // await web3.eth.sendTransaction({from: accounts[3], to: gid.address, value: toWei(1.5), gas: 3000000});
+        // balances[3] = await gid.balanceOf.call(accounts[3]);
+        // console.log(balances[3]);
+
+        balances[4] = await web3.eth.getBalance(accounts[4]);
+        console.log(balances[4]);
+
+        balances[5] = await web3.eth.getBalance(accounts[5]);
+        console.log(balances[5]);
     });
 });
-
-function toEther(wei) {
-    return wei / 1000000000000000000;
-}
 
 function toWei(ether) {
     return ether * 1000000000000000000;
 }
 
-function logBalance(mess, balance) {
-    console.log(mess, toEther(balance) + " ether");
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-// 621086 - 599552
